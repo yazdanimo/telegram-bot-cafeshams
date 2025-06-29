e6e736f0942eabd5afaaf8dd70a684f0bd6a2258
yazdanimo <yazdani.mohamad@gmail.com>
2025-06-29 22:29:58 +0330

    Create news_sources.py

---
+++ news_sources.py
@@ -0,0 +1,8 @@
+news_sources = [
+    {"name": "Reuters", "url": "https://feeds.reuters.com/reuters/topNews"},
+    {"name": "AP", "url": "https://feeds.apnews.com/apf-topnews"},
+    {"name": "BBC", "url": "https://feeds.bbci.co.uk/news/rss.xml"},
+    {"name": "IRNA", "url": "https://irna.ir/rss"},
+    {"name": "Fars", "url": "https://www.farsnews.ir/rss"},
+    # منابع دیگر...
+]
