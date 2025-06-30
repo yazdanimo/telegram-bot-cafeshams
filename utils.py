2baabe5c65a13a7752403cd2f332afae70c33e7a
yazdanimo <yazdani.mohamad@gmail.com>
2025-06-30 17:33:47 +0330

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,30 @@
+import aiohttp
+import asyncio
+from langdetect import detect
+
+def get_language(text):
+    try:
+        return detect(text)
+    except:
+        return "unknown"
+
+async def async_translate(text, target_lang="fa"):
+    url = "https://translate.googleapis.com/translate_a/single"
+    params = {
+        "client": "gtx",
+        "sl": "auto",
+        "tl": target_lang,
+        "dt": "t",
+        "q": text,
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params) as response:
+            if response.status == 200:
+                result = await response.json()
+                return "".join([item[0] for item in result[0]])
+            else:
+                return text
+
+def translate_text(text, dest="fa"):
+    return asyncio.run(async_translate(text, target_lang=dest))
