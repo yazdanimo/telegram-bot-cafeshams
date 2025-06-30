440cf59997ff07c8c604bcdb139a431a0195ec91
yazdanimo <yazdani.mohamad@gmail.com>
2025-06-30 19:56:37 +0330

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 GROUP_ID = -1002514471809  # آیدی عددی گروه سردبیری
 TOKEN = os.getenv("BOT_TOKEN")
 
-# بارگذاری منابع از فایل sources.json
 with open("sources.json", "r", encoding="utf-8") as f:
     sources = json.load(f)
 
 async def scheduled_job():
     await fetch_and_send_news(sources, bot, GROUP_ID)
 
-async def main():
+async def run_bot():
     application = ApplicationBuilder().token(TOKEN).build()
     global bot
     bot = application.bot
@@ -25,7 +24,12 @@ async def main():
     scheduler.start()
 
     print("🚀 ربات در حال اجراست...")
-    await application.run_polling()
+    await application.initialize()
+    await application.start()
+    await application.updater.start_polling()
+    # برای همیشه منتظر بمان
+    await asyncio.Event().wait()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.get_event_loop().create_task(run_bot())
+    asyncio.get_event_loop().run_forever()
