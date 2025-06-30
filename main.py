60478696273ab4b453e53775e44e7031d03d27a3
yazdanimo <yazdani.mohamad@gmail.com>
2025-06-30 13:37:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
 import os
-import asyncio
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from fetch_news import fetch_and_send_news
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-import nest_asyncio
+from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram import Update
+from telegram.ext import ContextTypes
 
-nest_asyncio.apply()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GROUP_ID = int(os.getenv("GROUP_ID"))
 
-TOKEN = os.getenv("BOT_TOKEN")
-GROUP_ID = int(os.getenv("GROUP_ID"))  # آیدی عددی گروه سردبیری
-
-async def start(update, context):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("✅ ربات خبری کافه شمس فعال است!")
 
-async def main():
-    app = ApplicationBuilder().token(TOKEN).build()
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(fetch_and_send_news, 'interval', seconds=60, args=[app, GROUP_ID])
-    scheduler.start()
-
     print("✅ ربات در حال اجراست و هر 1 دقیقه چک می‌کند...")
-    await app.run_polling(close_loop=False)
-
-if __name__ == '__main__':
-    asyncio.run(main())
+    app.run_polling()
