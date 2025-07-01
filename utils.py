bebe703d9b3752360ddea06d6589043aedf9f804
yazdanimo <yazdani.mohamad@gmail.com>
2025-07-01 18:03:14 +0330

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-
-def extract_image_from_html(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    img = soup.find('img')
-    if img and img.get('src'):
-        return img['src']
-    return None
