fdfbea7db6af06e190728442606d7194114c5d40
yazdanimo <yazdani.mohamad@gmail.com>
2025-07-07 14:00:57 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-import os
 import asyncio
 from telegram import Bot
 from fetch_news import fetch_and_send_news
 
-TOKEN = os.getenv("TOKEN", "7957685811:AAG_gzimHewoCWteEIf0mOcLDAnMgOu6Z3M")
-CHAT_ID = os.getenv("CHAT_ID", "-1002514471809")
+TOKEN   = "7957685811:AAG_gzimHewoCWteEIf0mOcLDAnMgOu6Z3M"
+CHAT_ID = "-1002514471809"
 
 bot = Bot(token=TOKEN)
 
 async def main_loop():
     sent_urls = set()
     while True:
+        print("✅ شروع دریافت و ارسال اخبار...")
         await fetch_and_send_news(bot, CHAT_ID, sent_urls)
-        await asyncio.sleep(10 * 60)  # هر ۱۰ دقیقه یکبار اجرا
+        print("🕒 پایان یک دور اجرا. صبر برای اجرای بعدی...")
+        await asyncio.sleep(600)  # هر ۱۰ دقیقه یک بار اجرا
 
 if __name__ == "__main__":
     asyncio.run(main_loop())
